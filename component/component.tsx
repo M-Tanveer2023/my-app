d68390afe8cf586de1eb59b2e0f97adcecc9acc0
M-Tanveer2023 <tanveer.tan90@gmail.com>
2023-05-11 02:36:44 +0500

    updated

--- component/component.tsx
+++ component/component.tsx
@@ -1,10 +1,12 @@
 const Component1 = ()=>{
     let a = "My first cariable Compoent"
+    let b = "My second cariable Compoent"
     return (
         <div>
             <h3>
                 {a}
             </h3>
+            <h3>{b}</h3>
         </div>
     )
 }
